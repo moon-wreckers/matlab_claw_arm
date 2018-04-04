dfdb74a2bb0eb5c7daaef98cc775966716e552db
David Qiu <david@davidqiu.com>
2018-04-03 23:23:53 -0400

    removed the conjugates by assuming real

--- src/pdirControl.m
+++ src/pdirControl.m
@@ -5,6 +5,12 @@ if ~exist('x_c_val', 'var')
 end
 
 syms theta1 theta2 x_c_x x_c_y x_c_z;
+assume(theta1, 'real');
+assume(theta2, 'real');
+assume(x_c_x, 'real');
+assume(x_c_y, 'real');
+assume(x_c_z, 'real');
+
 x_c = [x_c_x, x_c_y, x_c_z]';
 
 P = simClawArm(theta1, theta2);
@@ -21,9 +27,11 @@ loss = cross(dir_e, dir_c)' * cross(dir_e, dir_c) / 2;
 d_theta = [diff(loss, theta1);
            diff(loss, theta2)];
 
+d_theta = simplify(d_theta);
+
 % d_theta=
-%                                                                           - (((31*sin(conj(theta2))*(x_c_x - (3*sin(conj(theta1)))/40))/100 - (31*cos(conj(theta1))*cos(conj(theta2))*(x_c_z - 9/100))/100)*((93*cos(theta1)*sin(theta2))/4000 - (31*cos(theta2)*sin(theta1)*(conj(x_c_z) - 9/100))/100))/2 - (((31*sin(conj(theta2))*(x_c_y + (3*cos(conj(theta1)))/40))/100 - (31*cos(conj(theta2))*sin(conj(theta1))*(x_c_z - 9/100))/100)*((93*sin(theta1)*sin(theta2))/4000 + (31*cos(theta1)*cos(theta2)*(conj(x_c_z) - 9/100))/100))/2 - (((93*cos(conj(theta1))*sin(conj(theta2)))/4000 - (31*cos(conj(theta2))*sin(conj(theta1))*(x_c_z - 9/100))/100)*((31*sin(theta2)*(conj(x_c_x) - (3*sin(theta1))/40))/100 - (31*cos(theta1)*cos(theta2)*(conj(x_c_z) - 9/100))/100))/2 - (((93*sin(conj(theta1))*sin(conj(theta2)))/4000 + (31*cos(conj(theta1))*cos(conj(theta2))*(x_c_z - 9/100))/100)*((31*sin(theta2)*((3*cos(theta1))/40 + conj(x_c_y)))/100 - (31*cos(theta2)*sin(theta1)*(conj(x_c_z) - 9/100))/100))/2 - (((31*cos(conj(theta1))*cos(conj(theta2))*(x_c_y + (3*cos(conj(theta1)))/40))/100 - (31*cos(conj(theta2))*sin(conj(theta1))*(x_c_x - (3*sin(conj(theta1)))/40))/100)*((31*cos(theta1)*cos(theta2)*(conj(x_c_x) - (3*sin(theta1))/40))/100 + (31*cos(theta2)*sin(theta1)*((3*cos(theta1))/40 + conj(x_c_y)))/100))/2 - (((31*cos(conj(theta1))*cos(conj(theta2))*(x_c_x - (3*sin(conj(theta1)))/40))/100 + (31*cos(conj(theta2))*sin(conj(theta1))*(x_c_y + (3*cos(conj(theta1)))/40))/100)*((31*cos(theta1)*cos(theta2)*((3*cos(theta1))/40 + conj(x_c_y)))/100 - (31*cos(theta2)*sin(theta1)*(conj(x_c_x) - (3*sin(theta1))/40))/100))/2
-% (((31*cos(conj(theta2))*(x_c_x - (3*sin(conj(theta1)))/40))/100 + (31*cos(conj(theta1))*sin(conj(theta2))*(x_c_z - 9/100))/100)*((31*sin(theta2)*(conj(x_c_x) - (3*sin(theta1))/40))/100 - (31*cos(theta1)*cos(theta2)*(conj(x_c_z) - 9/100))/100))/2 + (((31*sin(conj(theta2))*(x_c_x - (3*sin(conj(theta1)))/40))/100 - (31*cos(conj(theta1))*cos(conj(theta2))*(x_c_z - 9/100))/100)*((31*cos(theta2)*(conj(x_c_x) - (3*sin(theta1))/40))/100 + (31*cos(theta1)*sin(theta2)*(conj(x_c_z) - 9/100))/100))/2 + (((31*cos(conj(theta2))*(x_c_y + (3*cos(conj(theta1)))/40))/100 + (31*sin(conj(theta1))*sin(conj(theta2))*(x_c_z - 9/100))/100)*((31*sin(theta2)*((3*cos(theta1))/40 + conj(x_c_y)))/100 - (31*cos(theta2)*sin(theta1)*(conj(x_c_z) - 9/100))/100))/2 + (((31*sin(conj(theta2))*(x_c_y + (3*cos(conj(theta1)))/40))/100 - (31*cos(conj(theta2))*sin(conj(theta1))*(x_c_z - 9/100))/100)*((31*cos(theta2)*((3*cos(theta1))/40 + conj(x_c_y)))/100 + (31*sin(theta1)*sin(theta2)*(conj(x_c_z) - 9/100))/100))/2 - (((31*cos(conj(theta1))*cos(conj(theta2))*(x_c_y + (3*cos(conj(theta1)))/40))/100 - (31*cos(conj(theta2))*sin(conj(theta1))*(x_c_x - (3*sin(conj(theta1)))/40))/100)*((31*cos(theta1)*sin(theta2)*((3*cos(theta1))/40 + conj(x_c_y)))/100 - (31*sin(theta1)*sin(theta2)*(conj(x_c_x) - (3*sin(theta1))/40))/100))/2 - (((31*cos(conj(theta1))*sin(conj(theta2))*(x_c_y + (3*cos(conj(theta1)))/40))/100 - (31*sin(conj(theta1))*sin(conj(theta2))*(x_c_x - (3*sin(conj(theta1)))/40))/100)*((31*cos(theta1)*cos(theta2)*((3*cos(theta1))/40 + conj(x_c_y)))/100 - (31*cos(theta2)*sin(theta1)*(conj(x_c_x) - (3*sin(theta1))/40))/100))/2
+% - ((31*sin(theta2)*(x_c_y + (3*cos(theta1))/40))/100 - cos(theta2)*sin(theta1)*((31*x_c_z)/100 - 279/10000))*((93*sin(theta1)*sin(theta2))/4000 - (279*cos(theta1)*cos(theta2))/10000 + (31*x_c_z*cos(theta1)*cos(theta2))/100) - ((31*sin(theta2)*(x_c_x - (3*sin(theta1))/40))/100 - cos(theta1)*cos(theta2)*((31*x_c_z)/100 - 279/10000))*((93*cos(theta1)*sin(theta2))/4000 + (279*cos(theta2)*sin(theta1))/10000 - (31*x_c_z*cos(theta2)*sin(theta1))/100) - (961*cos(theta2)^2*(x_c_x*cos(theta1) + x_c_y*sin(theta1))*(40*x_c_y*cos(theta1) - 40*x_c_x*sin(theta1) + 3))/400000
+%                                             ((31*cos(theta2)*(x_c_x - (3*sin(theta1))/40))/100 + cos(theta1)*sin(theta2)*((31*x_c_z)/100 - 279/10000))*((31*sin(theta2)*(x_c_x - (3*sin(theta1))/40))/100 - cos(theta1)*cos(theta2)*((31*x_c_z)/100 - 279/10000)) + ((31*cos(theta2)*(x_c_y + (3*cos(theta1))/40))/100 + sin(theta1)*sin(theta2)*((31*x_c_z)/100 - 279/10000))*((31*sin(theta2)*(x_c_y + (3*cos(theta1))/40))/100 - cos(theta2)*sin(theta1)*((31*x_c_z)/100 - 279/10000)) - (961*cos(theta2)*sin(theta2)*(40*x_c_y*cos(theta1) - 40*x_c_x*sin(theta1) + 3)^2)/16000000
 
 theta1 = theta1_val;
 theta2 = theta2_val;
--- src/runPDirControl.m
+++ src/runPDirControl.m
@@ -1,4 +1,4 @@
-function runPDirControl(x_c, alpha, x_0)
+function runPDirControl(x_c, theta, alpha, x_0)
 
 if ~exist('x_c', 'var')
     x_c = [1,1,1]';
@@ -12,8 +12,12 @@ if ~exist('x_0', 'var')
     x_0 = [0, 0, 0]';
 end
 
-theta1 = 0;
-theta2 = 0;
+if ~exist('theta', 'var')
+    theta = [0, 0];
+end
+    
+theta1 = theta(1);
+theta2 = theta(2);
 
 figure(1);
 visClawArm(theta1, theta2, x_0, 5);
@@ -21,10 +25,11 @@ hold on;
 visTarget(x_c, theta1, theta2, x_0);
 hold off;
 
-for t = 1:20
+for t = 1:50
     [d_theta1, d_theta2] = pdirControl(theta1, theta2, x_c);
     theta1 = theta1 - d_theta1 * alpha;
     theta2 = theta2 - d_theta2 * alpha;
+    fprintf('[theta1, theta2] = [%f, %f]\n', theta1, theta2);
     figure(1);
     visClawArm(theta1, theta2, x_0, 5);
     hold on;
